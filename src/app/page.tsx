2ed82257fcf646b4408d6c47d9ae94757a74bff5
okezieuc <okezieuc@users.noreply.github.com>
2023-10-28 19:47:17 -0500

    get URL for accessing the image after uploading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,51 +1,53 @@
-'use client'
+"use client";
 
-import { useState } from "react"
+import { useState } from "react";
 import firebaseApp from "@/utils/firebase";
-import { getStorage, ref, uploadBytes } from "firebase/storage";
-import { v4 as uuidv4 } from 'uuid';
-
+import { getDownloadURL, getStorage, ref, uploadBytes } from "firebase/storage";
+import { v4 as uuidv4 } from "uuid";
 
 // Initialize Cloud Storage and get a reference to the service
 const storage = getStorage(firebaseApp);
 
-
 export default function Home() {
   const [selectedFile, setSelectedFile] = useState<File>();
-
+  const [uploadedImageURL, setUploadedImageURL] = useState<string>();
 
   const uploadImage = () => {
     const randomFileName: string = uuidv4();
     const newImageRef = ref(storage, randomFileName);
 
     if (selectedFile != undefined) {
-      uploadBytes(newImageRef, selectedFile).then((snapshot) => {
-        console.log('Uploaded a blob or file!');
+      uploadBytes(newImageRef, selectedFile).then(() => {
+        console.log("Uploaded a blob or file!");
+        getDownloadURL(newImageRef).then((url) => {
+          setUploadedImageURL(url);
+        });
       });
-
     }
-
-
   };
 
   return (
     <main>
       Brain Cache
-
       <form>
         <input
           type="file"
           onChange={(e) => {
-            if (e.target.files) setSelectedFile(e.target.files[0])
+            if (e.target.files) setSelectedFile(e.target.files[0]);
           }}
         />
 
-        <button onClick={(e) => {
-          e.preventDefault()
-          uploadImage()
-        }}>Upload image</button>
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            uploadImage();
+          }}
+        >
+          Upload image
+        </button>
+
+        <img src={uploadedImageURL} />
       </form>
     </main>
-  )
+  );
 }
-
