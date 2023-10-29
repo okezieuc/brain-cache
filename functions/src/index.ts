806acc8420f08efd620b8c06107f63c4e9883e1b
okezieuc <okezieuc@users.noreply.github.com>
2023-10-29 00:06:48 -0500

    create brainCacheEntries after we upload image

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,13 +7,36 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-import {onRequest} from "firebase-functions/v2/https";
 import * as logger from "firebase-functions/logger";
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
 
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+exports.generateBrainCacheEntry = functions.storage
+  .object()
+  .onFinalize(async (object) => {
+    // Get the file metadata
+    const fileBucket = object.bucket;
+    const filePath = object.name;
+    const contentType = object.contentType; // File content type.
+
+    if (!(contentType && filePath)) {
+      return null;
+    }
+    // Exit if this is not an image file.
+    if (!contentType.startsWith("image/")) {
+      logger.error("This is not an image.");
+      return null;
+    }
+
+    // Log the download URL
+    logger.log("Path: ", fileBucket, filePath);
+
+    return admin
+      .firestore()
+      .collection("brainCacheEntries")
+      .doc()
+      .create({ imageData: { fileBucket, filePath } });
+  });
