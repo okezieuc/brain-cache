fbb6244e502d30fe8f97b0b00fb9e1f82b493d80
okezieuc <okezieuc@users.noreply.github.com>
2023-10-28 19:09:29 -0500

    create form for users to upload images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,23 @@
+'use client'
+
+import { useState } from "react"
+
 export default function Home() {
+  const [selectedFile, setSelectedFile] = useState<File>();
+
   return (
     <main>
       Brain Cache
+
+      <form>
+        <input
+          type="file"
+          onChange={(e) => {
+            if (e.target.files) setSelectedFile(e.target.files[0])
+          }}
+        />
+      </form>
     </main>
   )
 }
+
