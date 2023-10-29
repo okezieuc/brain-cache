398cff017c4e28e6b49e90b653f5e5f6ed8ac8f4
okezieuc <okezieuc@users.noreply.github.com>
2023-10-28 19:26:56 -0500

    upload files to firebase from the form input

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,29 @@
 'use client'
 
 import { useState } from "react"
+import firebaseApp from "@/utils/firebase";
+import { getStorage, ref, uploadBytes } from "firebase/storage";
+
+// Initialize Cloud Storage and get a reference to the service
+const storage = getStorage(firebaseApp);
+
 
 export default function Home() {
   const [selectedFile, setSelectedFile] = useState<File>();
+
+
+  const uploadImage = () => {
+    const newImageRef = ref(storage, 'file.jpg');
+
+    if (selectedFile != undefined) {
+      uploadBytes(newImageRef, selectedFile).then((snapshot) => {
+        console.log('Uploaded a blob or file!');
+      });
+
+    }
+
+
+  };
 
   return (
     <main>
@@ -16,6 +36,11 @@ export default function Home() {
             if (e.target.files) setSelectedFile(e.target.files[0])
           }}
         />
+
+        <button onClick={(e) => {
+          e.preventDefault()
+          uploadImage()
+        }}>Upload image</button>
       </form>
     </main>
   )
